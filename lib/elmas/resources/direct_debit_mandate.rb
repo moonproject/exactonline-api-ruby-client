5486500ffcf483035e2949039829e314db267acf
Damien Metzger <damien.metzger@gmail.com>
2018-09-26 12:15:15 +0200

    added direct_debit_mandate

---
+++ lib/elmas/resources/direct_debit_mandate.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Elmas
+  class DirectDebitMandate
+    # An account needs a name
+    include Elmas::Resource
+
+    def base_path
+      "cashflow/DirectDebitMandates"
+    end
+
+    def mandatory_attributes
+      %i[account bank_account]
+    end
+
+    # https://start.exactonline.nl/docs/HlpRestAPIResourcesDetails.aspx?name=CashflowDirectDebitMandates
+    def other_attributes
+      %i[description main payment_type reference signature_date type]
+    end
+  end
+end
